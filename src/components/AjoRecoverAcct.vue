e1d7ef5dac0d85c7f242b1eeaa595cb928753eae
saidabdul80 <saidabdulsalam5@gmail.com>
2024-09-20 17:30:30 +0100

    fix

--- src/components/AjoRecoverAcct.vue
+++ src/components/AjoRecoverAcct.vue
@@ -3,26 +3,21 @@
     <template1>
       <template #content>
         <ion-content color="white">
-          <div class="!tw-flex tw-flex-col tw-w-full tw-h-[100vh] tw-mt-20 tw-items-center">
-            <div class="tw-w-[50%] tw-mt-20">
+          <div class="!tw-flex tw-flex-col tw-w-full md:tw-h-[100vh]  tw-mt-20 tw-items-center">
+            <div class="md:tw-w-[50%] sm:tw-w-[70%] tw-w-[90%] md:tw-mt-20">
               <H1 class="tw-font-[600]" text="Recover account" />
               <span class="tw-text-[14.3px] tw-mb-8 tw-mt-2 tw-block tw-text-gray-500">
                 Please, enter your email address used for registration.
-                A code will be sent to verify you own the account. 
+                A code will be sent to verify you own the account.
               </span>
-              <Input 
-                v-model="form.email" 
-                class="tw-mb-5" 
-                placeholder="Email Address" 
-                size="medium" 
-              />
-              
-              <!-- Display error message if the email doesn't contain @gmail.com -->
+              <Input v-model="form.email" class="tw-mb-5" placeholder="Email Address" size="medium" />
+
+
               <span v-if="isEmailInvalid" class="tw-text-red-500 tw-text-[12px] tw-mb-4">
                 Please enter a valid email address.
               </span>
-              
-              
+
+
               <Button label="Continue" size="medium" class="tw-w-full tw-mt-[13px]  " />
             </div>
           </div>
@@ -34,36 +29,35 @@
 </template>
 
 
-    <script>
-    import { IonContent } from "@ionic/vue";
-    import Template1 from "@/components/Template1.vue";
-    import Input from "@/components/Input.vue";
-    import H1 from "@/components/H1.vue";
-    import Button from "@/components/Button.vue";
+<script>
+import { IonContent } from "@ionic/vue";
+import Template1 from "@/components/Template1.vue";
+import Input from "@/components/Input.vue";
+import H1 from "@/components/H1.vue";
+import Button from "@/components/Button.vue";
 
-    export default {
-        components: {
-            IonContent,
-            Template1,
-            Input,
-            H1,
-            Button,
-        },
-        data() {
+export default {
+  components: {
+    IonContent,
+    Template1,
+    Input,
+    H1,
+    Button,
+  },
+  data() {
     return {
       form: {
-        email: '', // Track the email input
+        email: '',
       },
     };
   },
   computed: {
-    // Check if the email does not contain @gmail.com
+
     isEmailInvalid() {
       return !this.form.email.includes('@gmail.com') && this.form.email.length > 0;
     }
   },
-    };
-    </script>
+};
+</script>
 
-    <style lang="scss" scoped>
-    </style>
+<style lang="scss" scoped></style>
