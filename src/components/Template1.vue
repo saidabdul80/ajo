369d8e2e8bdf159ea89347bc8b40cfc41626a1d1
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2024-09-20 22:10:16 +0100

    feat(Homepage) - Adjust layout design for home page

--- src/components/Template1.vue
+++ src/components/Template1.vue
@@ -1,36 +1,48 @@
 <template>
   <IonContent class="md:tw-hidden tw-block">
-        <slot name="content"></slot>
+    <slot name="content"></slot>
   </IonContent>
-  <div class="md:tw-grid tw-grid-cols-7 tw-w-full tw-hidden  ">
+  <div class="md:tw-grid tw-grid-cols-7 tw-w-full tw-hidden tw-h-full">
     <div class="tw-col-span-3 tw-bg-black">
-        <div class=" tw-px-8">
-            <div class="tw-w-full tw-py-6 tw-mb-8">
-                <img src="@/assets/images/logo-white.svg" />
-            </div>
-            <div
-            class="tw-mb-8 tw-bg-[url('/images/bar-bg.svg')] tw-bg-no-repeat tw-bg-cover tw-w-full tw-h-[43vh] tw-flex tw-justify-center tw-items-center"
-            >
-            <div class="tw-bg-white tw-w-[95%] tw-h-[60%] tw-rounded-md"></div>
-            </div>
-            <div class="tw-text-white tw-w-[80%] tw-mx-auto tw-mb-20">
-                <p class="tw-m-0 tw-text-2xl tw-text-center">Save<span class="box tw-mx-1"><span class="tw-text-black tw-inline-block tw-relative tw-left-1 tw-z-10">60% of your time</span></span></p>
-                <p class="tw-m-0 tw-text-2xl tw-text-center">managing group finances.</p>
-                <p class="tw-m-0 tw-text-xs tw-text-center">Cowris simplifies the process of group savings, contributions, and financial management for your circle—be it friends, family, or colleagues. Track contributions, manage funds, and reach your financial goals together with ease</p>
-            </div>
+      <div class="tw-px-8">
+        <div class="tw-w-full tw-py-6 tw-mb-8">
+          <img src="@/assets/images/logo-white.svg" />
         </div>
-      <div class="tw-bg-[url('/images/coin-bg.svg')] tw-bg-no-repeat  tw-h-[14vh] tw-bg-cover tw-w-full"></div>
+        <div
+          class="tw-mb-8 tw-bg-[url('/images/bar-bg.svg')] tw-bg-no-repeat tw-bg-cover tw-w-full tw-h-[43vh] tw-flex tw-justify-center tw-items-center">
+          <div class="tw-bg-white tw-w-[95%] tw-h-[60%] tw-rounded-md"></div>
+        </div>
+        <div class="tw-text-white tw-w-[80%] tw-mx-auto tw-mb-20">
+          <p class="tw-m-0 tw-text-2xl tw-text-center">
+            Save<span class="box tw-mx-1"
+              ><span
+                class="tw-text-black tw-inline-block tw-relative tw-left-1 tw-z-10"
+                >60% of your time</span
+              ></span
+            >
+          </p>
+          <p class="tw-m-0 tw-text-2xl tw-text-center">
+            managing group finances.
+          </p>
+          <p class="tw-m-0 tw-text-xs tw-text-center">
+            Cowris simplifies the process of group savings, contributions, and
+            financial management for your circle—be it friends, family, or
+            colleagues. Track contributions, manage funds, and reach your
+            financial goals together with ease
+          </p>
+        </div>
+      </div>
+      <div
+        class="tw-bg-[url('/images/coin-bg.svg')] tw-bg-no-repeat tw-h-[14vh] tw-bg-cover tw-w-full"></div>
     </div>
-    <div class="tw-col-span-4 ">
-        <slot name="content"></slot>
+    <div class="tw-col-span-4">
+      <slot name="content"></slot>
     </div>
-    
   </div>
-  
 </template>
 
 <script>
-import { IonContent } from '@ionic/vue';
+import { IonContent } from "@ionic/vue";
 
 export default {
   components: {
--- src/views/Join.vue
+++ src/views/Join.vue
@@ -1,57 +1,78 @@
 <template>
-    <div class="tw-h-full tw-w-full" color="light">
-        <template1>
-            <template #content>
-                <ion-content color="white" >
-                    <div class="!tw-flex tw-flex-col tw-w-full md:tw-h-[100vh]  tw-mt-20 tw-items-center">
-                        <div class="md:tw-w-[50%] sm:tw-w-[70%] tw-w-[90%] md:tw-mt-20">
-                            <H1 class="md:tw-text-left tw-text-center" text="Join Ajo by Cowris" />
-                            <span class="md:tw-text-left  tw-text-balance tw-text-center tw-text-sm tw-mb-8 tw-block tw-text-gray-500">Already have an account on Cowris? <a @click="$globals.to('/signin')" class="tw-underline tw-cursor-pointer tw-text-gray-800"> Sign in </a></span>
-                            <Input class="tw-mb-5" placeholder="Full Name" size="medium" />
-                            <Input class="tw-mb-5" placeholder="Email Address" size="medium" />
-                            <Password class="tw-mb-8" placeholder="Password" size="medium" />
-                            <div class="tw-flex tw-w-full tw-gap-1 tw-mb-8">
-                                <Checkbox :binary="true" v-model="form.accept"/>
-                                <p class="tw-text-sm -tw-mt-1 tw-block tw-text-gray-500 tw-text-balance">
-                                    I have read and agreed to the <a class="tw-underline tw-cursor-pointer tw-text-gray-800">Terms & Conditions</a> 
-                                    and <a class="tw-underline tw-cursor-pointer tw-text-gray-800">KYC Policy</a> and confirm that I am opening this account for my own personal use, and not for use by a third party.
-                                </p>
-                            </div>
-                            <Button label="Sign up" size="medium" class="tw-w-full"/>
-                        </div>
-                    </div>
-                </ion-content>
-            </template>
-        </template1>
-    </div>
+  <div class="tw-h-full tw-w-full" color="light">
+    <template1>
+      <template #content>
+        <ion-content color="white">
+          <div
+            class="!tw-flex tw-flex-col tw-w-full tw-pt-20 md:tw-pt-40 tw-items-center">
+            <div class="md:tw-w-[50%] sm:tw-w-[70%] tw-w-[90%] md:tw-mt-20">
+              <H1
+                class="md:tw-text-left tw-text-center"
+                text="Join Ajo by Cowris" />
+              <span
+                class="md:tw-text-left tw-text-balance tw-text-center tw-text-sm tw-mb-8 tw-block tw-text-gray-500"
+                >Already have an account on Cowris?
+                <a
+                  @click="$globals.to('/signin')"
+                  class="tw-underline tw-cursor-pointer tw-text-gray-800">
+                  Sign in
+                </a></span
+              >
+              <Input class="tw-mb-5" placeholder="Full Name" size="medium" />
+              <Input
+                class="tw-mb-5"
+                placeholder="Email Address"
+                size="medium" />
+              <Password class="tw-mb-8" placeholder="Password" size="medium" />
+              <div class="tw-flex tw-w-full tw-gap-1 tw-mb-8">
+                <Checkbox :binary="true" v-model="form.accept" />
+                <p
+                  class="tw-text-sm -tw-mt-1 tw-block tw-text-gray-500 tw-text-balance">
+                  I have read and agreed to the
+                  <a class="tw-underline tw-cursor-pointer tw-text-gray-800"
+                    >Terms & Conditions</a
+                  >
+                  and
+                  <a class="tw-underline tw-cursor-pointer tw-text-gray-800"
+                    >KYC Policy</a
+                  >
+                  and confirm that I am opening this account for my own personal
+                  use, and not for use by a third party.
+                </p>
+              </div>
+              <Button label="Sign up" size="medium" class="tw-w-full" />
+            </div>
+          </div>
+        </ion-content>
+      </template>
+    </template1>
+  </div>
 </template>
 
 <script>
-import {IonContent} from "@ionic/vue";
+import { IonContent } from "@ionic/vue";
 import Template1 from "@/components/Template1.vue";
-import Input from "@/components/Input.vue"
-import Password from "@/components/Password.vue"
-import Checkbox from "@/components/Checkbox.vue"
-import H1 from "@/components/H1.vue"
-import Button from "@/components/Button.vue"
-    export default {
-        components: {
-            IonContent,
-            Template1,
-            Input,
-            Password,
-            H1,
-            Checkbox,
-            Button
-        },
-        data() {
-            return {
-                form:{}
-            }
-        },
-    }
+import Input from "@/components/Input.vue";
+import Password from "@/components/Password.vue";
+import Checkbox from "@/components/Checkbox.vue";
+import H1 from "@/components/H1.vue";
+import Button from "@/components/Button.vue";
+export default {
+  components: {
+    IonContent,
+    Template1,
+    Input,
+    Password,
+    H1,
+    Checkbox,
+    Button,
+  },
+  data() {
+    return {
+      form: {},
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
