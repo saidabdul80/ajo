0c4d035fe123dd55491dcc1225d33035ac240019
Said Abdulsalam <sabdulsalam@ubtconsults.se>
2024-09-20 18:33:29 +0100

    fix: update

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,7 +19,7 @@ const routes: Array<RouteRecordRaw> = [
   },
   
   {
-    path: '/Verify',
+    path: '/verify',
     name: 'Verify',
     component: () => import('@/views/VerifyEmail.vue')
   },
