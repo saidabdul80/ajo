3361beb8da4c878663ac6b3425885fcc250a36ca
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2024-11-22 16:57:39 +0100

    refactor: adjust code structure

--- src/views/Passwordset.vue
+++ src/views/Passwordset.vue
@@ -3,31 +3,17 @@
     <template1>
       <template #content>
         <ion-content color="white">
-          <div
-            class="tw-flex tw-flex-col tw-w-full tw-items-center md:tw-justify-center tw-h-full tw-pt-20 md:tw-py-0">
+          <div class="tw-flex tw-flex-col tw-w-full tw-items-center md:tw-justify-center tw-h-full tw-pt-20 md:tw-py-0">
             <div class="lg:tw-w-[50%] sm:tw-w-[70%] tw-w-[90%]">
-              <div
-                class="tw-mx-auto tw-w-[150px] tw-h-[150px] md:tw-w-[300px] md:tw-h-[300px] tw-mb-12">
-                <img
-                  src="/images/password-protection.svg"
-                  alt="password set icon"
-                  width="100%"
-                  height="100%" />
+              <div class="tw-mx-auto tw-w-[150px] tw-h-[150px] md:tw-w-[300px] md:tw-h-[300px] tw-mb-12">
+                <img src="/images/password-protection.svg" alt="password set icon" width="100%" height="100%" />
               </div>
 
               <div class="tw-text-center tw-mx-auto">
                 <H1 class="tw-text-center" text="Password set!" />
-                <p
-                  class="tw-pt-4 tw-pb-12 tw-text-lg tw-text-[#586283] tw-font-normal">
-                  Your new password has been successfully created. You can now
-                  sign in with your new password.
-                </p>
+                <p class="tw-pt-4 tw-pb-12 tw-text-lg tw-text-[#586283] tw-font-normal">Your new password has been successfully created. You can now sign in with your new password.</p>
               </div>
-              <Button
-                label="Sign in"
-                size="large"
-                class="tw-w-full"
-                @click="$globals.to('/signin')" />
+              <Button label="Sign in" size="large" class="tw-w-full" @click="$globals.to('/signin')" />
             </div>
           </div>
         </ion-content>
