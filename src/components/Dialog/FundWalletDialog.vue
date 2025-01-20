28b778590f130d70656a74a9d50be7abab3129e4
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2025-01-20 13:33:34 +0100

    update - wallet balance tag UI changes

--- src/components/Dialog/FundWalletDialog.vue
+++ src/components/Dialog/FundWalletDialog.vue
@@ -1,22 +1,16 @@
 <template>
   <div class="tw-flex tw-items-center tw-flex-col tw-gap-3">
-    <img
-      src="@/assets/images/logo.svg"
-      alt="logo"
-      class="tw-h-20 tw-w-20 tw-inline-block" />
-      <div class="tw-text-black tw-text-2xl tw-flex tw-items-baseline tw-gap-1">
+    <img src="@/assets/images/logo.svg" alt="logo" class="tw-h-20 tw-w-20 tw-inline-block" />
+    <div class="tw-text-black tw-text-2xl tw-flex tw-items-baseline tw-gap-1">
+      {{ $globals.formatNumber(userStore.user?.my_wallet?.balance || 0) }}
+      <span class="tw-text-sm">{{ userStore.user.my_wallet.currency }}</span>
+    </div>
+    <p class="tw-px-3 tw-py-1 tw-bg-[#DAE3FF] tw-rounded-lg">Wallet Balance</p>
 
-        {{ $globals.formatNumber(userStore.user?.my_wallet?.balance || 0) }}
-        <span class="tw-text-sm">{{ userStore.user.my_wallet.currency }}</span>
-      </div>
-    <Button :is-full-width="false" label="Wallet Balance" />
-
-    <div v-if="!transactionInitiated" class="tw-mt-10 ">
-      <p class="tw-text-[#333333] tw-mb-4 tw-text-center">
-        To fund your wallet, click the button below to generate your transaction reference and bank details.
-      </p>
+    <div v-if="!transactionInitiated" class="tw-mt-10">
+      <p class="tw-text-[#333333] tw-mb-4 tw-text-center">To fund your wallet, click the button below to generate your transaction reference and bank details.</p>
       <label class="tw-text-sm tw-w-full !tw-text-left tw-text-[#333333]">Gateway</label>
-      <Select  v-model="selectedGateWay" class="tw-w-full tw-mb-2" placeholder="Select a Gateway" :options="gateways" optionLabel="name" optionValue="value" /> 
+      <Select v-model="selectedGateWay" class="tw-w-full tw-mb-2" placeholder="Select a Gateway" :options="gateways" optionLabel="name" optionValue="value" />
       <label class="tw-text-sm tw-w-full !tw-text-left tw-text-[#333333]">Amount</label>
       <InputNumber v-model="amount" placeholder="Enter Amount" class="tw-mb-2" inputId="integeronly" fluid />
       <Button :loading="loading" @click="initiateTransaction" label="Initiate Transaction" />
@@ -28,33 +22,24 @@
       </div>
 
       <div>
-        <h4 class="tw-text-lg tw-text-black tw-font-semibold">
-          Cowris Account Details
-        </h4>
-        <p class="tw-text-[#333333]">
-          Send money to the account details below. Once the money transfer is
-          successful, it will reflect on your wallet balance.
-        </p>
+        <h4 class="tw-text-lg tw-text-black tw-font-semibold">Cowris Account Details</h4>
+        <p class="tw-text-[#333333]">Send money to the account details below. Once the money transfer is successful, it will reflect on your wallet balance.</p>
         <ul class="tw-py-8 tw-flex tw-flex-col tw-gap-4">
-          <li
-            v-for="(value, key) in fundingDetails"
-            :key="key"
-            class="tw-flex tw-justify-between tw-items-center">
+          <li v-for="(value, key) in fundingDetails" :key="key" class="tw-flex tw-justify-between tw-items-center">
             <p class="tw-text-[#636363]">{{ formatKey(key) }}</p>
             <TextClipboard :text="value" />
           </li>
           <li class="tw-flex tw-justify-between tw-items-center">
             <p class="tw-text-[#636363]">Remark Narration:</p>
             <TextClipboard :text="reference" />
-            </li>
+          </li>
         </ul>
-    
+
         <Button @click="closeDialog" label="Close" />
       </div>
     </div>
   </div>
 </template>
-
 
 <script>
 import eventBus from "@/eventBus";
@@ -63,7 +48,7 @@ import Input from "@/components/Input.vue";
 import Divider from "primevue/divider";
 import TextClipboard from "../TextClipboard.vue";
 import { useClient } from "@/stores/client";
-import {useUserStore} from "@/stores/user";
+import { useUserStore } from "@/stores/user";
 import InputNumber from "primevue/inputnumber";
 import Select from "primevue/select";
 export default {
@@ -73,7 +58,7 @@ export default {
     TextClipboard,
     Input,
     Select,
-    InputNumber
+    InputNumber,
   },
 
   data() {
@@ -94,36 +79,35 @@ export default {
           value: "FIAT_MATCH",
         },
       ],
-      amount:0,
+      amount: 0,
     };
   },
 
   methods: {
     formatKey(key) {
-      return key
-        .replace(/([A-Z])/g, " $1")
-        .replace(/^./, (str) => str.toUpperCase());
+      return key.replace(/([A-Z])/g, " $1").replace(/^./, (str) => str.toUpperCase());
     },
 
     async initiateTransaction() {
       try {
-        
-        if(this.amount == 0){
-          this.$globals.ubtAlert({title:"Please enter an amount"});
+        if (this.amount == 0) {
+          this.$globals.ubtAlert({ title: "Please enter an amount" });
           return;
         }
         this.loading = true;
-        const response = await useClient().http({method:'post', path:"/transactions/initiate", data:{ 
-          gateway: this.selectedGateWay,
-          type: "deposit",
-          currency: "CAD",
-          channel: "E_TRANSFER",
-          amount:this.amount,
-        }
-
-      });
-      this.loading = false;
-      if(response){
+        const response = await useClient().http({
+          method: "post",
+          path: "/transactions/initiate",
+          data: {
+            gateway: this.selectedGateWay,
+            type: "deposit",
+            currency: "CAD",
+            channel: "E_TRANSFER",
+            amount: this.amount,
+          },
+        });
+        this.loading = false;
+        if (response) {
           this.fundingDetails = {
             accountNumber: response.number,
             bankName: response.bank_name,
@@ -131,8 +115,7 @@ export default {
           };
           this.reference = response.reference;
           this.transactionInitiated = true;
-
-      }
+        }
       } catch (error) {
         console.error("Error initiating transaction:", error);
         alert("Failed to initiate transaction. Please try again.");
